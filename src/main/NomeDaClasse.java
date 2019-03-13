1874be2d64d35722c949199d21a237858483526e
Tiago Freire <titomanimal@gmail.com>
2019-03-13 18:23:46 +0000

    commitando proibidamente

---
+++ src/main/NomeDaClasse.java
@@ -0,0 +1,20 @@
+
+
+/**
+ * Classe que representa ...
+ * 
+ * @author Tiago Freire
+ * @version 1.0
+ * @since 2019-03-13
+ *
+ */
+ 
+public class NomeDaClasse {
+
+	/**
+	 * Construtor
+	 **/
+	public NomeDaClasse (){
+	}
+
+}
